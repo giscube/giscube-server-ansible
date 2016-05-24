8bb9b85f3024da8d0ffddd3946da4c9e1a8eac90
Manel Clos <manelclos@gmail.com>
2016-05-24 14:22:47 +0200

    output information about what is going on

--- giscube-install-local.sh
+++ giscube-install-local.sh
@@ -1,4 +1,13 @@
 #! /bin/sh
+DIR=$(cd $(dirname "$0"); pwd)
+cd $DIR
+echo "* Executing script from $DIR"
+echo
+
 . $PWD/envvars.sh
+echo "* Installing dependencies"
 sh install-deps.sh
+echo
+
+echo "* Installing giscube using ansible"
 ansible-playbook giscube.yml
