319523e8f7ab1c3d34db63e0236a80d8e486f56d
Manel Clos <manelclos@gmail.com>
2016-05-24 14:22:17 +0200

    add system dependencies

--- install-deps.sh
+++ install-deps.sh
@@ -1,1 +1,7 @@
+#! /bin/sh
+echo "- Install python-pip"
+apt-get install -q -y python-pip python-dev libffi-dev libssl-dev
+echo "- Install ansible globally"
+pip -q -U install ansible setuptools
+echo "- Install required roles from ansible galaxy"
 ansible-galaxy install -r requirements.yml -p ./roles/
