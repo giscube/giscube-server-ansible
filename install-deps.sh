5fb83617ad37372e8f700956cf4174d659308fef
Alexandre <abusquets@gmail.com>
2016-06-22 00:31:39 +0200

    module markupsafe dependency
    ERROR! Unexpected Exception: No module named markupsafe

--- install-deps.sh
+++ install-deps.sh
@@ -3,5 +3,7 @@ echo "- Install python-pip"
 apt-get install -q -y python-pip python-dev libffi-dev libssl-dev
 echo "- Install ansible globally"
 pip -q -U install ansible setuptools
+echo "- Install markupsafe globally"
+pip install markupsafe
 echo "- Install required roles from ansible galaxy"
 ansible-galaxy install -r requirements.yml -p ./roles/
