118b2b9efc90ba1396d29d35802e0b65d2b76c9c
Alexandre Busquets <abusquets@gmail.com>
2016-08-30 09:49:54 +0200

    Add apt requirements

--- install-deps.sh
+++ install-deps.sh
@@ -1,4 +1,6 @@
 #! /bin/sh
+echo "- Install apt requirements"
+apt-get install -q aptitude
 echo "- Install python-pip"
 apt-get install -q -y python-pip python-dev libffi-dev libssl-dev
 echo "- Install ansible globally"
