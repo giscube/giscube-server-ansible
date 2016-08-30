3c3defb3379d9b80740064b38f22759e492d1a04
Alexandre Busquets <abusquets@gmail.com>
2016-08-30 12:04:23 +0200

    Fix pip install upgrade option

--- install-deps.sh
+++ install-deps.sh
@@ -4,6 +4,6 @@ apt-get install -q aptitude
 echo "- Install python-pip"
 apt-get install -q -y python-pip python-dev libffi-dev libssl-dev
 echo "- Install ansible globally"
-pip -q -U install ansible setuptools
+pip install -q -U ansible setuptools
 echo "- Install required roles from ansible galaxy"
 ansible-galaxy install -r requirements.yml -p ./roles/
